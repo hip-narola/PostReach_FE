edf12dbbfb7dd218ced038ac4f825adc880f0e1c
Himani <hip@narola.email>
2025-01-18 17:19:28 +0530

    Changes for cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 // middleware.ts
 import {NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-// import { LocalStorageType } from './app/constants/pages';
+import { LocalStorageType } from './app/constants/pages';
 import navigations from './app/constants/navigations';
 
 export function middleware(request: NextRequest) {
   
   const isAuthPage = request.nextUrl.pathname.startsWith(navigations.protectedRoute);
   // Redirect unauthenticated requests to login
-  if (isAuthPage /*&& !request.cookies.get(LocalStorageType.ACCESS_TOKEN)*/) {
+  if (isAuthPage && !request.cookies.get(LocalStorageType.ACCESS_TOKEN)) {
     return NextResponse.redirect(new URL(navigations.login, request.url));
   }
   return NextResponse.next();
